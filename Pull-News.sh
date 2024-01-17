00cf43cdceb7b310ee3351e3d758388fae57eaf6
rune <christian.rune1998@gmail.com>
2024-01-17 15:09:00 +0100

    Testing

--- Pull-News.sh
+++ Pull-News.sh
@@ -176,17 +176,12 @@ sudo docker stop $DOCKER_IMAGE_NAME
 sudo docker rm $DOCKER_IMAGE_NAME
 
 
-# Validate the input is a number or if it's empty
-if ! [[ "$host_port" =~ ^[0-9]+$ ]] && [[ -n "$host_port" ]]; then
-    echo "Invalid port number. Please enter a numeric value."
-    exit 1
-fi
 
 # Set the port to 8383 if no input was provided
-host_port=${host_port:-8383}
+host_port=8383
 
 # Spin up a new Docker container using the new image
-sudo docker run -d --name $DOCKER_IMAGE_NAME -p $host_port:80 $DOCKER_IMAGE_NAME
+sudo docker run -d --name $DOCKER_IMAGE_NAME -p $host_port:8383 $DOCKER_IMAGE_NAME
 
 # Remove the used HTML file
 rm $NEWEST_HTML_FILE
